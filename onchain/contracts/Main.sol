d6355b96e1fe4329a9712515f10036293ef291e4
JustAgent <baykov.ivan.03@gmail.com>
2023-01-01 21:50:27 +0300

    sign func

--- onchain/contracts/Main.sol
+++ onchain/contracts/Main.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Main is ERC20, Ownable {
+
     constructor(
         string memory _name,
         string memory _symbol
@@ -15,24 +16,27 @@ contract Main is ERC20, Ownable {
         // uint256 orderId;
         address seller;
         address buyer;
+        address provider;
         uint256 energyAmount;
         uint256 pricePerUnit;
         uint256 totalPrice;
         uint256 deliveryDate;
         bool buyerSign;
         bool sellerSign;
-        Status fullfilmentStatus;
+        Status status;
     }
 
     enum Status {
-        Fulfilled,
-        InProcess,
-        OnVerification,
-        Declined
+        Fulfilled, // Order completed
+        InProcess,  // Order submitted and currently in work
+        OnVerification, // Waiting for signs
+        Declined, // Someone declined
+        Prepayment // Waiting for prepayment
     }
 
     mapping(uint256 => Order) public orders;
-    mapping(address => bool) public intermediaries;
+    mapping(address => bool) public providers;
+
     uint totalOrders;
 
     function createOrder(
@@ -42,11 +46,13 @@ contract Main is ERC20, Ownable {
         uint _pricePerUnit,
         uint _deliveryDate
     ) external {
+        require(providers[msg.sender] == true, "Only for providers");
         uint orderId = totalOrders + 1;
         uint totalPrice = _energyAmount * _pricePerUnit;
         orders[orderId] = Order(
             _seller,
             _buyer,
+            msg.sender,
             _energyAmount,
             _pricePerUnit,
             totalPrice,
@@ -55,6 +61,34 @@ contract Main is ERC20, Ownable {
             false,
             Status.OnVerification
         );
+    }
+
+    function signOrder(uint orderId, uint8 side) external returns(bool) {
+        require(orderId <= totalOrders,"Invalid order ID");
+        require(side == 0 || side == 1, "Select a correct side");
+        Order storage order = orders[orderId]; //debug
+
+        if (side == 0) {
+            require(order.buyer == msg.sender,"Only the buyer can sign this order");
+            order.buyerSign = true;
+            if (order.sellerSign == true) {
+                order.status = Status.Prepayment;
+            }
+            return true;
+        }
+        if (side == 1) {
+            require(order.seller == msg.sender,"Only the seller can sign this order");
+            order.sellerSign = true;
+            if (order.buyerSign == true) {
+                order.status = Status.Prepayment;
+            }
+            return true;
+        }
+        return false;
+    } 
+
+    function getOrder(uint orderId) public view returns (Order memory) {
+        return orders[orderId];
     }
 
 
